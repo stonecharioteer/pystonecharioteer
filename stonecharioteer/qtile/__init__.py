7a0ce1376eef30ef063b72c3005df4cfb3dad236
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-11-05 13:01:57 +0530

    add keymaps

--- stonecharioteer/qtile/__init__.py
+++ stonecharioteer/qtile/__init__.py
@@ -4,16 +4,31 @@
 def config():
     """This function modifies the global variables, injecting
     parameters required to customize qtile."""
-    
+
     from libqtile.utils import guess_terminal
     from stonecharioteer.qtile.groups import groups
     from stonecharioteer.qtile.inputs import MOD
     from stonecharioteer.qtile.layouts import layouts
     from stonecharioteer.qtile.screens import configure_screens
 
+    screens = configure_screens()
     globals()["mod"] = MOD
     globals()["groups"] = groups
     globals()["terminal"] = guess_terminal()
     globals()["layouts"] = layouts
-    globals()["screens"] = configure_screens()
-    
+    globals()["screens"] = screens
+    globals()["keys"] = configure_keymaps(groups=groups)
+
+    # FIXME: Remove these later, or investigate if they're required.
+    dgroups_key_binder = None
+    globals()["dgroups_key_binder"] = dgroups_key_binder
+    dgroups_app_rules = []  # type: List
+    globals()["dgroups_app_rules"] = dgroups_app_rules
+    follow_mouse_focus = True
+    globals()["follow_mouse_focus"] = follow_mouse_focus
+    bring_front_click = False
+    globals()["bring_front_click"] = bring_front_click
+    cursor_warp = False
+    globals()["cursor_warp"] = cursor_warp
+    auto_minimize = True
+    globals()["auto_minimize"] = auto_minimize
---
+++ stonecharioteer/qtile/keymaps.py
@@ -0,0 +1,106 @@
+from libqtile.lazy import lazy
+from libqtile.config import Key
+from stonecharioteer.qtile.inputs import Keyboard, MOD
+
+
+def configure_keymaps(groups):
+    """Configures keymaps"""
+    keymaps = []
+
+    MODIFIER_SET_1 = [MOD]
+    MODIFIER_SET_2 = [MOD, Keyboard.SHIFT]
+    MODIFIER_SET_3 = [MOD, Keyboard.CTRL]
+    # Movement keymaps
+    keys = [
+        # Switch between windows
+        Key(MODIFIER_SET_1, "h", lazy.layout.left(), desc="Move focus to left"),
+        Key(MODIFIER_SET_1, "l", lazy.layout.right(), desc="Move focus to right"),
+        Key(MODIFIER_SET_1, "j", lazy.layout.down(), desc="Move focus down"),
+        Key(MODIFIER_SET_1, "k", lazy.layout.up(), desc="Move focus up"),
+        Key(
+            MODIFIER_SET_1,
+            "space",
+            lazy.layout.next(),
+            desc="Move window focus to other window",
+        ),
+        # Move windows between left/right columns or move up/down in current stack.
+        # Moving out of range in Columns layout will create new column.
+        Key(
+            MODIFIER_SET_2,
+            "h",
+            lazy.layout.shuffle_left(),
+            desc="Move window to the left",
+        ),
+        Key(
+            MODIFIER_SET_2,
+            "l",
+            lazy.layout.shuffle_right(),
+            desc="Move window to the right",
+        ),
+        Key(MODIFIER_SET_2, "j", lazy.layout.shuffle_down(), desc="Move window down"),
+        Key(MODIFIER_SET_2, "k", lazy.layout.shuffle_up(), desc="Move window up"),
+        # Grow windows. If current window is on the edge of screen and direction
+        # will be to screen edge - window would shrink.
+        Key(
+            MODIFIER_SET_3, "h", lazy.layout.grow_left(), desc="Grow window to the left"
+        ),
+        Key(
+            MODIFIER_SET_3,
+            "l",
+            lazy.layout.grow_right(),
+            desc="Grow window to the right",
+        ),
+        Key(MODIFIER_SET_3, "j", lazy.layout.grow_down(), desc="Grow window down"),
+        Key(MODIFIER_SET_3, "k", lazy.layout.grow_up(), desc="Grow window up"),
+        Key(
+            MODIFIER_SET_1, "n", lazy.layout.normalize(), desc="Reset all window sizes"
+        ),
+        # Toggle between split and unsplit sides of stack.
+        # Split = all windows displayed
+        # Unsplit = 1 window displayed, like Max layout, but still with
+        # multiple stack panes
+        Key(
+            MODIFIER_SET_2,
+            "Return",
+            lazy.layout.toggle_split(),
+            desc="Toggle between split and unsplit sides of stack",
+        ),
+        Key(MODIFIER_SET_1, "Return", lazy.spawn(terminal), desc="Launch terminal"),
+        # Toggle between different layouts as defined below
+        Key(MODIFIER_SET_1, "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+        Key(MODIFIER_SET_1, "w", lazy.window.kill(), desc="Kill focused window"),
+        Key(MODIFIER_SET_3, "r", lazy.reload_config(), desc="Reload the config"),
+        Key(MODIFIER_SET_3, "q", lazy.shutdown(), desc="Shutdown Qtile"),
+        Key(
+            MODIFIER_SET_1,
+            "r",
+            lazy.spawncmd(),
+            desc="Spawn a command using a prompt widget",
+        ),
+    ]
+
+    for ix, group in enumerate(groups):
+        keys.extend(
+            [
+                Key(
+                    MODIFIER_SET_1,
+                    str(ix + 1),
+                    lazy.group[group.name].toscreen(),
+                    desc=f"Switch to group {group.name}",
+                ),
+                Key(
+                    MODIFIER_SET_2,
+                    str(ix + 1),
+                    lazy.window.togroup(group.name, switch_group=True),
+                    desc=f"Switch to & move focussed window to group {group.name}",
+                ),
+                Key(
+                    MODIFIER_SET_3,
+                    str(ix + 1),
+                    lazy.window.togroup(group.name),
+                    desc=f"Move focussed window to group {group.name}",
+                ),
+            ]
+        )
+
+    return keys
