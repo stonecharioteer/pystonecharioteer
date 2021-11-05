704d582a2203b595381fed619726831eb7695b56
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-11-05 14:51:02 +0530

    add logger

---
+++ stonecharioteer/logger.py
@@ -0,0 +1,26 @@
+import logging
+import logging.handlers
+import pathlib
+
+logger = logging.getLogger("stonecharioteer")
+
+
+def configure_logger():
+    logfile = pathlib.Path("~/.logs/stonecharioteer.log")
+
+    logfile.parent.mkdir(parents=True, exist_ok=True)
+    # TODO: Read config and set the log level.
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.handlers.RotatingFileHandler(
+        filename=logfile.absolute(), maxBytes=1024 * 1024
+    )
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    stream_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+    logger.debug("Configured the logger")
--- stonecharioteer/qtile/__init__.py
+++ stonecharioteer/qtile/__init__.py
@@ -1,35 +1,50 @@
 """QTile configuration submodule"""
+from stonecharioteer.logger import logger, configure_logger
 
 
 def config():
     """This function modifies the global variables, injecting
     parameters required to customize qtile."""
-
+    configure_logger()
+    logger.debug("Importing dependencies.")
     from libqtile.utils import guess_terminal
+
+    logger.debug("Importing custom items")
     from stonecharioteer.qtile.groups import groups
     from stonecharioteer.qtile.inputs import MOD
     from stonecharioteer.qtile.layouts import layouts
     from stonecharioteer.qtile.screens import configure_screens
     from stonecharioteer.qtile.keymaps import configure_keymaps
 
+    logger.debug("Building screens")
+    configurables = {}
     screens = configure_screens()
-    globals()["mod"] = MOD
-    globals()["groups"] = groups
-    globals()["terminal"] = guess_terminal()
-    globals()["layouts"] = layouts
-    globals()["screens"] = screens
-    globals()["keys"] = configure_keymaps(groups=groups)
 
+    logger.debug("Setting `mod` key")
+    configurables["mod"] = MOD
+    logger.debug("Setting `groups`")
+    configurables["groups"] = groups
+    logger.debug("Setting `terminal`")
+    configurables["terminal"] = guess_terminal()
+    logger.debug("Setting `layouts`")
+    configurables["layouts"] = layouts
+    logger.debug("Setting `screens`")
+    configurables["screens"] = screens
+    logger.debug("Setting `keys`")
+    configurables["keys"] = configure_keymaps(groups=groups)
+
+    logger.debug("Setting misc. items.")
     # FIXME: Remove these later, or investigate if they're required.
     dgroups_key_binder = None
-    globals()["dgroups_key_binder"] = dgroups_key_binder
+    configurables["dgroups_key_binder"] = dgroups_key_binder
     dgroups_app_rules = []  # type: List
-    globals()["dgroups_app_rules"] = dgroups_app_rules
+    configurables["dgroups_app_rules"] = dgroups_app_rules
     follow_mouse_focus = True
-    globals()["follow_mouse_focus"] = follow_mouse_focus
+    configurables["follow_mouse_focus"] = follow_mouse_focus
     bring_front_click = False
-    globals()["bring_front_click"] = bring_front_click
+    configurables["bring_front_click"] = bring_front_click
     cursor_warp = False
-    globals()["cursor_warp"] = cursor_warp
+    configurables["cursor_warp"] = cursor_warp
     auto_minimize = True
-    globals()["auto_minimize"] = auto_minimize
+    configurables["auto_minimize"] = auto_minimize
+    return configurables
