a5d3364a3dea97af6171ce5ec3c02e8e4dcbf206
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-12-02 11:31:12 +0530

    update to 0.2.5

--- stonecharioteer/qtile/screens.py
+++ stonecharioteer/qtile/screens.py
@@ -4,7 +4,7 @@ are.
 
 from libqtile.config import Screen
 from stonecharioteer.qtile.panels import default_top_bar, default_bottom_bar
-from stonecharioteer.utils.display import get_display_info
+from stonecharioteer.utils.displays import get_display_info
 
 
 def configure_screens():
@@ -21,7 +21,7 @@ def configure_screens():
     if len(displays) > 1:
         for display in displays:
             display_name = display["name"]
-            if display_name == "eDP":
+            if display_name != "eDP":
                 continue
             else:
                 screen = Screen(bottom=default_bottom_bar)
