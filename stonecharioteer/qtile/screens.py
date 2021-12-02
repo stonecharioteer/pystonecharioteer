512a794c4c52760e407260655ba186f4fc2b88c0
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-12-02 11:27:27 +0530

    update version to 0.2.4 -  add multi-monitor support

--- stonecharioteer/qtile/screens.py
+++ stonecharioteer/qtile/screens.py
@@ -1,36 +1,29 @@
-"""This module configures the monitors depending on how many displays
-there are. To properly use it, you need to create a config file in
-~/.config/stonecharioteer/monitors.toml, note that that file will
-need to list the IDs of your monitors, getting them from `xrandr`
-so that this code can properly configure them. Alternatively, you can
-overrride the location of the monitors config file using the
-`STONECHARIOTEER_MONITORS` environment variable, which should
-point to the *absolute* path of a toml file."""
-
-import pathlib
-import os
-import toml
+"""This module configures the monitors depending on how many displays there
+are.
+"""
 
 from libqtile.config import Screen
 from stonecharioteer.qtile.panels import default_top_bar, default_bottom_bar
+from stonecharioteer.utils.display import get_display_info
+
 
 def configure_screens():
     """This function configures the screens based on how many active
     monitors there are currently."""
-    if monitors_config := os.environ.get("STONECHARIOTEER_MONITORS"):
-        monitors_config = pathlib.Path(monitors_config)
-    else:
-        monitors_config = pathlib.Path("~/.config/stonecharioteer/monitors.toml")
-    if monitors_config.is_file():
-        with open(monitors_config) as f:
-            monitor_config = toml.load(f)
-        # TODO: implement behavior for multi monitor configuration 
-    
+    displays = get_display_info()
+
     default_screen = Screen(
-            top = default_top_bar,
-            bottom= default_bottom_bar,
-            )
-    screens = [
-        default_screen
-    ]
+        top=default_top_bar,
+        bottom=default_bottom_bar,
+    )
+
+    screens = [default_screen]
+    if len(displays) > 1:
+        for display in displays:
+            display_name = display["name"]
+            if display_name == "eDP":
+                continue
+            else:
+                screen = Screen(bottom=default_bottom_bar)
+                screens.append(screen)
     return screens
