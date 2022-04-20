b20b968f216a831c48e34142c092bf7f0bac07dd
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-04-20 10:31:50 +0530

    chore: update variable names for keymap modifiers so they are more obvious

--- stonecharioteer/qtile/keymaps.py
+++ stonecharioteer/qtile/keymaps.py
@@ -8,19 +8,19 @@ from stonecharioteer.utils.displays import get_display_info
 
 def configure_keymaps(groups):
     """Configures keymaps"""
-    terminal = "/bin/konsole"
-    MODIFIER_SET_1 = [MOD.value]
-    MODIFIER_SET_2 = [MOD.value, Keyboard.SHIFT.value]
-    MODIFIER_SET_3 = [MOD.value, Keyboard.CTRL.value]
+    terminal = "/home/stonecharioteer/.local/bin/kitty"
+    Super = [MOD.value]
+    SuperShift = [MOD.value, Keyboard.SHIFT.value]
+    SuperControl = [MOD.value, Keyboard.CTRL.value]
     # Movement keymaps
     keys = [
         # Switch between windows
-        Key(MODIFIER_SET_1, "h", lazy.layout.left(), desc="Move focus to left"),
-        Key(MODIFIER_SET_1, "l", lazy.layout.right(), desc="Move focus to right"),
-        Key(MODIFIER_SET_1, "j", lazy.layout.down(), desc="Move focus down"),
-        Key(MODIFIER_SET_1, "k", lazy.layout.up(), desc="Move focus up"),
+        Key(Super, "h", lazy.layout.left(), desc="Move focus to left"),
+        Key(Super, "l", lazy.layout.right(), desc="Move focus to right"),
+        Key(Super, "j", lazy.layout.down(), desc="Move focus down"),
+        Key(Super, "k", lazy.layout.up(), desc="Move focus up"),
         Key(
-            MODIFIER_SET_1,
+            Super,
             "space",
             lazy.layout.next(),
             desc="Move window focus to other window",
@@ -28,55 +28,49 @@ def configure_keymaps(groups):
         # Move windows between left/right columns or move up/down in current stack.
         # Moving out of range in Columns layout will create new column.
         Key(
-            MODIFIER_SET_2,
+            SuperShift,
             "h",
             lazy.layout.shuffle_left(),
             desc="Move window to the left",
         ),
         Key(
-            MODIFIER_SET_2,
+            SuperShift,
             "l",
             lazy.layout.shuffle_right(),
             desc="Move window to the right",
         ),
-        Key(MODIFIER_SET_2, "j", lazy.layout.shuffle_down(), desc="Move window down"),
-        Key(MODIFIER_SET_2, "k", lazy.layout.shuffle_up(), desc="Move window up"),
+        Key(SuperShift, "j", lazy.layout.shuffle_down(), desc="Move window down"),
+        Key(SuperShift, "k", lazy.layout.shuffle_up(), desc="Move window up"),
         # Grow windows. If current window is on the edge of screen and direction
         # will be to screen edge - window would shrink.
+        Key(SuperControl, "h", lazy.layout.grow_left(), desc="Grow window to the left"),
         Key(
-            MODIFIER_SET_3, "h", lazy.layout.grow_left(), desc="Grow window to the left"
-        ),
-        Key(
-            MODIFIER_SET_3,
+            SuperControl,
             "l",
             lazy.layout.grow_right(),
             desc="Grow window to the right",
         ),
-        Key(MODIFIER_SET_3, "j", lazy.layout.grow_down(), desc="Grow window down"),
-        Key(MODIFIER_SET_3, "k", lazy.layout.grow_up(), desc="Grow window up"),
-        Key(
-            MODIFIER_SET_1, "n", lazy.layout.normalize(), desc="Reset all window sizes"
-        ),
+        Key(SuperControl, "j", lazy.layout.grow_down(), desc="Grow window down"),
+        Key(SuperControl, "k", lazy.layout.grow_up(), desc="Grow window up"),
+        Key(Super, "n", lazy.layout.normalize(), desc="Reset all window sizes"),
         # Toggle between split and unsplit sides of stack.
         # Split = all windows displayed
         # Unsplit = 1 window displayed, like Max layout, but still with
         # multiple stack panes
         Key(
-            MODIFIER_SET_2,
+            SuperShift,
             "Return",
             lazy.layout.toggle_split(),
             desc="Toggle between split and unsplit sides of stack",
         ),
-        Key(MODIFIER_SET_1, "Return", lazy.spawn(terminal), desc="Launch terminal"),
+        Key(Super, "Return", lazy.spawn(terminal), desc="Launch terminal"),
         # Toggle between different layouts as defined below
-        Key(MODIFIER_SET_1, "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-        Key(MODIFIER_SET_1, "w", lazy.window.kill(), desc="Kill focused window"),
+        Key(Super, "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+        Key(Super, "w", lazy.window.kill(), desc="Kill focused window"),
+        Key(SuperControl, "r", lazy.restart(), desc="Restart Qtile and reload config"),
+        Key(SuperControl, "q", lazy.shutdown(), desc="Shutdown Qtile"),
         Key(
-            MODIFIER_SET_3, "r", lazy.restart(), desc="Restart Qtile and reload config"
-        ),
-        Key(MODIFIER_SET_3, "q", lazy.shutdown(), desc="Shutdown Qtile"),
-        Key(
-            MODIFIER_SET_1,
+            Super,
             "r",
             lazy.spawn("""rofi -modi "drun,run,window,ssh" -show drun"""),
             desc="Spawn a command using a prompt widget",
@@ -87,19 +81,19 @@ def configure_keymaps(groups):
         keys.extend(
             [
                 Key(
-                    MODIFIER_SET_1,
+                    Super,
                     str(ix + 1),
                     lazy.group[group.name].toscreen(),
                     desc=f"Switch to group {group.name}",
                 ),
                 Key(
-                    MODIFIER_SET_2,
+                    SuperShift,
                     str(ix + 1),
                     lazy.window.togroup(group.name, switch_group=True),
                     desc=f"Switch to & move focussed window to group {group.name}",
                 ),
                 Key(
-                    MODIFIER_SET_3,
+                    SuperControl,
                     str(ix + 1),
                     lazy.window.togroup(group.name),
                     desc=f"Move focussed window to group {group.name}",
@@ -113,7 +107,7 @@ def configure_keymaps(groups):
         display_keys = [
             ("u", "eDP"),
             # FIXME: This could be DisplayPort-0 or DisplayPort-1
-            ("y", "DisplayPort-0"),
+            ("m", "DisplayPort-0"),
         ]
     elif len(displays) == 3:
         display_keys = ["u", "i", "y"]
@@ -134,7 +128,7 @@ def configure_keymaps(groups):
             jerry_command = "jerry -m {}".format(display)
             logger.warning("Monitor = {}".format(display))
             logger.debug(jerry_command)
-            keys.append(Key(MODIFIER_SET_1, key, lazy.spawn(jerry_command)))
+            keys.append(Key(Super, key, lazy.spawn(jerry_command)))
     except:
         pass
 
