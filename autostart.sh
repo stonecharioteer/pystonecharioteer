70b715f67e69d7172d6ef7dd79c7ebaa19f8f5b8
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2023-05-17 12:03:17 +0530

    feat: fix layouts, only use what's required, bump to 0.7.6

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-pasystray &
-nitrogen --restore &
-xscreensaver &
-xfce4-power-manager &
-dunst &
+flock /tmp/pasystray.lock --command "pasystray" &
+flock /tmp/nitrogen.lock --command "nitrogen --restore" &
+flock /tmp/xscreensaver.lock --command "xscreensaver" &
+# flock /tmp/xfce4-power-manager.lock --command "xfce4-power-manager" &
+flock /tmp/dunst.lock --command "dunst" &
+flock /tmp/picom.lock --command "picom" &
+flock /tmp/rog.lock --command "rog-control-center" &
+flock /tmp/shutter.lock --command "shutter --min_at_startup" &
+flock /tmp/redshift.lock --command "redshift -t 5700:3600" &
